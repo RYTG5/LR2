1251a2018e836c5e332f9e5035abf80372e1e297
RYTG5 <121175259+RYTG5@users.noreply.github.com>
2023-01-17 16:15:41 +0200

    Update BridgeBeam.py

--- BridgeBeam.py
+++ BridgeBeam.py
@@ -1,4 +1,4 @@
-﻿import NemAll_Python_Geometry as AllplanGeo
+import NemAll_Python_Geometry as AllplanGeo
 import NemAll_Python_BaseElements as AllplanBaseElements
 import NemAll_Python_BasisElements as AllplanBasisElements
 import NemAll_Python_Utility as AllplanUtil
@@ -9,161 +9,164 @@ from HandleDirection import HandleDirection
 from HandleProperties import HandleProperties
 from HandleService import HandleService
 
-print('Load CreateBridgeBeam.py')
+def check_allplan_version(buildEl, version):
 
-
-def check_allplan_version(build_ele, version):
-    # Delete unused arguments
-    del build_ele
+    del buildEl
     del version
 
-    # Support all versions
     return True
 
 
-def create_element(build_ele, doc):
-    # Create Element
+def create_element(buildEl, doc):
+
     element = CreateBridgeBeam(doc)
 
-    # Return a tuple with elements list and handles list
-    return element.create(build_ele)
+
+    return element.create(buildEl)
 
 
-def move_handle(build_ele, handle_prop, input_pnt, doc):
-    # Change the handle properties
-    build_ele.change_property(handle_prop, input_pnt)
+def move_handle(buildEl, handle_prop, input_pnt, doc):
 
-    if handle_prop.handle_id == "BeamHeight":
-        build_ele.RibHeight.value = build_ele.BeamHeight.value - build_ele.TopShHeight.value - \
-                                    build_ele.BotShLowHeight.value - build_ele.BotShUpHeight.value
-
-        if build_ele.HoleHeight.value > build_ele.BeamHeight.value - build_ele.TopShHeight.value - 45.5:
-            build_ele.HoleHeight.value = build_ele.BeamHeight.value - build_ele.TopShHeight.value - 45.5
-
-    # Recreate element with new properties
-    return create_element(build_ele, doc)
+    buildEl.change_property(handle_prop, input_pnt)
 
 
-def modify_element_property(build_ele, name, value):
-    # Handle dependencies for changed property
-    # Return true/false for palette refresh
+    check_equality(handle_prop.handle_id, buildEl)
+    check_Height(buildEl)
+
+
+    return create_element(buildEl, doc)
+
+def check_equality(handle_id,buildEl):
+    if handle_id == "BeamHeight":
+        buildEl.RibHeight.value = buildEl.BeamHeight.value - buildEl.TopShHeight.value - \
+                                buildEl.BotShLowHeight.value - buildEl.BotShUpHeight.value
+
+def check_Height(buildEl):
+    if buildEl.HoleHeight.value > buildEl.BeamHeight.value - buildEl.TopShHeight.value - 45.5:
+            buildEl.HoleHeight.value = buildEl.BeamHeight.value - buildEl.TopShHeight.value - 45.5
+
+def change_property(buildEl, name, value):
+
     if name == "BeamHeight":
-        change = value - build_ele.TopShHeight.value - build_ele.RibHeight.value - \
-                 build_ele.BotShUpHeight.value - build_ele.BotShLowHeight.value
+        change = value - buildEl.TopShHeight.value - buildEl.RibHeight.value - \
+                 buildEl.BotShUpHeight.value - buildEl.BotShLowHeight.value
 
         print(change)
         if change < 0:
             change = abs(change)
-            if build_ele.TopShHeight.value > 320.:
-                if build_ele.TopShHeight.value - change < 320.:
-                    change -= build_ele.TopShHeight.value - 320.
-                    build_ele.TopShHeight.value = 320.
+            if buildEl.TopShHeight.value > 320.:
+                if buildEl.TopShHeight.value - change < 320.:
+                    change -= buildEl.TopShHeight.value - 320.
+                    buildEl.TopShHeight.value = 320.
                 else:
-                    build_ele.TopShHeight.value -= change
+                    buildEl.TopShHeight.value -= change
                     change = 0.
-            if (change != 0) and (build_ele.BotShUpHeight.value > 160.):
-                if build_ele.BotShUpHeight.value - change < 160.:
-                    change -= build_ele.BotShUpHeight.value - 160.
-                    build_ele.BotShUpHeight.value = 160.
+            if (change != 0) and (buildEl.BotShUpHeight.value > 160.):
+                if buildEl.BotShUpHeight.value - change < 160.:
+                    change -= buildEl.BotShUpHeight.value - 160.
+                    buildEl.BotShUpHeight.value = 160.
                 else:
-                    build_ele.BotShUpHeight.value -= change
+                    buildEl.BotShUpHeight.value -= change
                     change = 0.
-            if (change != 0) and (build_ele.BotShLowHeight.value > 153.):
-                if build_ele.BotShLowHeight.value - change < 153.:
-                    change -= build_ele.BotShLowHeight.value - 153.
-                    build_ele.BotShLowHeight.value = 153.
+            if (change != 0) and (buildEl.BotShLowHeight.value > 153.):
+                if buildEl.BotShLowHeight.value - change < 153.:
+                    change -= buildEl.BotShLowHeight.value - 153.
+                    buildEl.BotShLowHeight.value = 153.
                 else:
-                    build_ele.BotShLowHeight.value -= change
+                    buildEl.BotShLowHeight.value -= change
                     change = 0.
-            if (change != 0) and (build_ele.RibHeight.value > 467.):
-                if build_ele.RibHeight.value - change < 467.:
-                    change -= build_ele.RibHeight.value - 467.
-                    build_ele.RibHeight.value = 467.
+            if (change != 0) and (buildEl.RibHeight.value > 467.):
+                if buildEl.RibHeight.value - change < 467.:
+                    change -= buildEl.RibHeight.value - 467.
+                    buildEl.RibHeight.value = 467.
                 else:
-                    build_ele.RibHeight.value -= change
+                    buildEl.RibHeight.value -= change
                     change = 0.
         else:
-            build_ele.RibHeight.value += change
-        if value - build_ele.TopShHeight.value - 45.5 < build_ele.HoleHeight.value:
-            build_ele.HoleHeight.value = value - build_ele.TopShHeight.value - 45.5
-    elif name == "TopShHeight":
-        build_ele.BeamHeight.value = value + build_ele.RibHeight.value + \
-                                     build_ele.BotShUpHeight.value + build_ele.BotShLowHeight.value
-    elif name == "RibHeight":
-        build_ele.BeamHeight.value = value + build_ele.TopShHeight.value + \
-                                     build_ele.BotShUpHeight.value + build_ele.BotShLowHeight.value
-    elif name == "BotShUpHeight":
-        build_ele.BeamHeight.value = value + build_ele.TopShHeight.value + \
-                                     build_ele.RibHeight.value + build_ele.BotShLowHeight.value
-        if value + build_ele.BotShLowHeight.value + 45.5 > build_ele.HoleHeight.value:
-            build_ele.HoleHeight.value = value + build_ele.BotShLowHeight.value + 45.5
-    elif name == "BotShLowHeight":
-        build_ele.BeamHeight.value = value + build_ele.TopShHeight.value + \
-                                     build_ele.RibHeight.value + build_ele.BotShUpHeight.value
-        if build_ele.BotShUpHeight.value + value + 45.5 > build_ele.HoleHeight.value:
-            build_ele.HoleHeight.value = build_ele.BotShUpHeight.value + value + 45.5
-    elif name == "HoleHeight":
-        if value > build_ele.BeamHeight.value - build_ele.TopShHeight.value - 45.5:
-            build_ele.HoleHeight.value = build_ele.BeamHeight.value - build_ele.TopShHeight.value - 45.5
-        elif value < build_ele.BotShLowHeight.value + build_ele.BotShUpHeight.value + 45.5:
-            build_ele.HoleHeight.value = build_ele.BotShLowHeight.value + build_ele.BotShUpHeight.value + 45.5
-    elif name == "HoleDepth":
-        if value >= build_ele.BeamLength.value / 2.:
-            build_ele.HoleDepth.value = build_ele.BeamLength.value / 2. - 45.5
+            buildEl.RibHeight.value += change
+        if value - buildEl.TopShHeight.value - 45.5 < buildEl.HoleHeight.value:
+            buildEl.HoleHeight.value = value - buildEl.TopShHeight.value - 45.5
+    else:
+        Switch(buildEl,name,value)
 
     return True
 
+def Switch(buildEl,name,value):
+    if name == "TopShHeight":
+        buildEl.BeamHeight.value = value + buildEl.RibHeight.value + \
+                                     buildEl.BotShUpHeight.value + buildEl.BotShLowHeight.value
+    if name == "RibHeight":
+        buildEl.BeamHeight.value = value + buildEl.TopShHeight.value + \
+                                     buildEl.BotShUpHeight.value + buildEl.BotShLowHeight.value
+    if name == "BotShUpHeight":
+        buildEl.BeamHeight.value = value + buildEl.TopShHeight.value + \
+                                     buildEl.RibHeight.value + buildEl.BotShLowHeight.value
+        if value + buildEl.BotShLowHeight.value + 45.5 > buildEl.HoleHeight.value:
+            buildEl.HoleHeight.value = value + buildEl.BotShLowHeight.value + 45.5
+    if name == "BotShLowHeight":
+        buildEl.BeamHeight.value = value + buildEl.TopShHeight.value + \
+                                     buildEl.RibHeight.value + buildEl.BotShUpHeight.value
+        if buildEl.BotShUpHeight.value + value + 45.5 > buildEl.HoleHeight.value:
+            buildEl.HoleHeight.value = buildEl.BotShUpHeight.value + value + 45.5
+    if name == "HoleHeight":
+        if value > buildEl.BeamHeight.value - buildEl.TopShHeight.value - 45.5:
+            buildEl.HoleHeight.value = buildEl.BeamHeight.value - buildEl.TopShHeight.value - 45.5
+        elif value < buildEl.BotShLowHeight.value + buildEl.BotShUpHeight.value + 45.5:
+            buildEl.HoleHeight.value = buildEl.BotShLowHeight.value + buildEl.BotShUpHeight.value + 45.5
+    if name == "HoleDepth":
+        if value >= buildEl.BeamLength.value / 2.:
+            buildEl.HoleDepth.value = buildEl.BeamLength.value / 2. - 45.5
 
 class CreateBridgeBeam:
     def __init__(self, doc):
-        self.model_ele_list = []
+        self.El_list = []
         self.handle_list = []
         self.document = doc
 
-    def create(self, build_ele):
-        self._top_sh_width = build_ele.TopShWidth.value
-        self._top_sh_height = build_ele.TopShHeight.value
+    def create(self, buildEl):
+        self._top_sh_width = buildEl.TopShWidth.value
+        self._top_sh_height = buildEl.TopShHeight.value
 
-        self._bot_sh_width = build_ele.BotShWidth.value
-        self._bot_sh_up_height = build_ele.BotShUpHeight.value
-        self._bot_sh_low_height = build_ele.BotShLowHeight.value
+        self._bot_sh_width = buildEl.BotShWidth.value
+        self._bot_sh_up_height = buildEl.BotShUpHeight.value
+        self._bot_sh_low_height = buildEl.BotShLowHeight.value
         self._bot_sh_height = self._bot_sh_up_height + self._bot_sh_low_height
 
-        if build_ele.RibThick.value > min(self._top_sh_width, self._bot_sh_width):
-            build_ele.RibThick.value = min(self._top_sh_width, self._bot_sh_width)
-        self._rib_thickness = build_ele.RibThick.value
-        self._rib_height = build_ele.RibHeight.value
+        if buildEl.RibThick.value > min(self._top_sh_width, self._bot_sh_width):
+            buildEl.RibThick.value = min(self._top_sh_width, self._bot_sh_width)
+        self._rib_thickness = buildEl.RibThick.value
+        self._rib_height = buildEl.RibHeight.value
 
-        self._beam_length = build_ele.BeamLength.value
+        self._beam_length = buildEl.BeamLength.value
         self._beam_width = max(self._top_sh_width, self._bot_sh_width)
-        self._beam_height = build_ele.BeamHeight.value
+        self._beam_height = buildEl.BeamHeight.value
 
-        self._hole_depth = build_ele.HoleDepth.value
-        self._hole_height = build_ele.HoleHeight.value
+        self._hole_depth = buildEl.HoleDepth.value
+        self._hole_height = buildEl.HoleHeight.value
 
-        self._angleX = build_ele.RotationAngleX.value
-        self._angleY = build_ele.RotationAngleY.value
-        self._angleZ = build_ele.RotationAngleZ.value
+        self._angleX = buildEl.RotationAngleX.value
+        self._angleY = buildEl.RotationAngleY.value
+        self._angleZ = buildEl.RotationAngleZ.value
 
-        self.create_beam(build_ele)
-        self.create_handles(build_ele)
+        self.create_beam(buildEl)
+        self.create_handles(buildEl)
 
         AllplanBaseElements.ElementTransform(AllplanGeo.Vector3D(), self._angleX, self._angleY, self._angleZ,
-                                             self.model_ele_list)
+                                             self.El_list)
 
         rot_angles = RotationAngles(self._angleX, self._angleY, self._angleZ)
         HandleService.transform_handles(self.handle_list, rot_angles.get_rotation_matrix())
 
-        return self.model_ele_list, self.handle_list
+        return self.El_list, self.handle_list
 
-    def create_beam(self, build_ele):
+    def create_beam(self, buildEl):
         com_prop = AllplanBaseElements.CommonProperties()
         com_prop.GetGlobalProperties()
         com_prop.Pen = 1
-        com_prop.Color = build_ele.Color3.value
+        com_prop.Color = buildEl.Color3.value
         com_prop.Stroke = 1
 
-        # bottom shelf
+
         bottom_shelf = AllplanGeo.BRep3D.CreateCuboid(
             AllplanGeo.AxisPlacement3D(AllplanGeo.Point3D((self._beam_width - self._bot_sh_width) / 2., 0., 0.),
                                        AllplanGeo.Vector3D(1, 0, 0), AllplanGeo.Vector3D(0, 0, 1)), self._bot_sh_width,
@@ -174,7 +177,7 @@ class CreateBridgeBeam:
         edges.append(8)
         err, bottom_shelf = AllplanGeo.ChamferCalculus.Calculate(bottom_shelf, edges, 20., False)
 
-        # top shelf
+
         top_shelf = AllplanGeo.BRep3D.CreateCuboid(AllplanGeo.AxisPlacement3D(
             AllplanGeo.Point3D((self._beam_width - self._top_sh_width) / 2., 0.,
                                self._beam_height - self._top_sh_height), AllplanGeo.Vector3D(1, 0, 0),
@@ -195,7 +198,7 @@ class CreateBridgeBeam:
         if not GeometryValidate.polyhedron(err):
             return
 
-        # rib
+
         rib = AllplanGeo.BRep3D.CreateCuboid(
             AllplanGeo.AxisPlacement3D(AllplanGeo.Point3D(0., 0., self._bot_sh_height), AllplanGeo.Vector3D(1, 0, 0),
                                        AllplanGeo.Vector3D(0, 0, 1)), self._beam_width, self._beam_length,
@@ -205,7 +208,7 @@ class CreateBridgeBeam:
         if not GeometryValidate.polyhedron(err):
             return
 
-        # left and right notches
+
         left_notch_pol = AllplanGeo.Polygon2D()
         left_notch_pol += AllplanGeo.Point2D((self._beam_width - self._rib_thickness) / 2.,
                                              self._beam_height - self._top_sh_height)
@@ -222,7 +225,7 @@ class CreateBridgeBeam:
 
         path = AllplanGeo.Polyline3D()
         path += AllplanGeo.Point3D(0, 0, 0)
-        path += AllplanGeo.Point3D(0, build_ele.BeamLength.value, 0)
+        path += AllplanGeo.Point3D(0, buildEl.BeamLength.value, 0)
 
         err, notches = AllplanGeo.CreatePolyhedron(left_notch_pol, AllplanGeo.Point2D(0., 0.), path)
         if GeometryValidate.polyhedron(err):
@@ -247,10 +250,10 @@ class CreateBridgeBeam:
             if not GeometryValidate.polyhedron(err):
                 return
 
-        # sling_holes
+
         sling_holes = AllplanGeo.BRep3D.CreateCylinder(
             AllplanGeo.AxisPlacement3D(
-                AllplanGeo.Point3D(0, build_ele.HoleDepth.value, build_ele.HoleHeight.value),
+                AllplanGeo.Point3D(0, buildEl.HoleDepth.value, buildEl.HoleHeight.value),
                 AllplanGeo.Vector3D(0, 0, 1), AllplanGeo.Vector3D(1, 0, 0)),
             45.5,
             self._beam_width
@@ -268,10 +271,10 @@ class CreateBridgeBeam:
         if not GeometryValidate.polyhedron(err):
             return
 
-        # result beam
-        self.model_ele_list.append(AllplanBasisElements.ModelElement3D(com_prop, beam))
 
-    def create_handles(self, build_ele):
+        self.El_list.append(AllplanBasisElements.ModelElement3D(com_prop, beam))
+        
+    def create_handle1(self):
         handle1 = HandleProperties(
             "BeamLength",
             AllplanGeo.Point3D(0., self._beam_length, 0.),
@@ -281,6 +284,7 @@ class CreateBridgeBeam:
         )
         self.handle_list.append(handle1)
 
+    def create_handle2(self):
         handle2 = HandleProperties(
             "BeamHeight",
             AllplanGeo.Point3D(0., 0., self._beam_height),
@@ -290,6 +294,7 @@ class CreateBridgeBeam:
         )
         self.handle_list.append(handle2)
 
+    def create_handle3(self):
         handle3 = HandleProperties(
             "TopShWidth",
             AllplanGeo.Point3D(
@@ -300,7 +305,7 @@ class CreateBridgeBeam:
             HandleDirection.point_dir, True
         )
         self.handle_list.append(handle3)
-
+    def create_handle4(self):
         handle4 = HandleProperties(
             "BotShWidth",
             AllplanGeo.Point3D(
@@ -313,6 +318,7 @@ class CreateBridgeBeam:
         )
         self.handle_list.append(handle4)
 
+    def create_handle5(self):
         handle5 = HandleProperties(
             "RibThick",
             AllplanGeo.Point3D(
